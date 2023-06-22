3e14e7093194523281c9aa02a9f1db4c263a080a
dev-tch <tijeni.chaabeni@gmail.com>
2023-06-22 18:05:54 +0000

    add program 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * _isupper - check if a character is an uppercase
+ *@c: input character
+ * Return: 1 or 0
+ */
+
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
+}
