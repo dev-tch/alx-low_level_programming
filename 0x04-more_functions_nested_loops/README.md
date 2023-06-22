7a7b5e8fc2d9ebef7faadc2957095c26d7185380
dev-tch <tijeni.chaabeni@gmail.com>
2023-06-22 18:49:33 +0000

    prog4

--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,5 @@
 - program 1-isdigit.c
 - program 2-mul.c
 - program 3-print_numbers.c
+- program 4-print_most_numbers.c
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,10 @@
+#define MYFUNCTIONS_H notused
+#ifndef MYFUNCTIONS_H
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+#endif
 int _putchar(char c);
+void print_most_numbers(void);
+
