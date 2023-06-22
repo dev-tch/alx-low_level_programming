23f49d4fb72ee1fd1505105c99e2c8a25f36f375
dev-tch <tijeni.chaabeni@gmail.com>
2023-06-22 19:55:18 +0000

    new test prog4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * print_numbers - from 0 to 9
+ * print_most_numbers - from 0 to 9 execpt 2 && 4
  *
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int i;
 for (i = 0; i <= 9 ; i++)
 {
-if (i != 2 || i != 4)
+if (i != 2 && i != 4)
 {
 _putchar(i + 48);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,7 @@
-#define MYFUNCTIONS_H notused
-#ifndef MYFUNCTIONS_H
-int _isupper(int c);
+/*int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void);
-#endif
+void print_numbers(void);*/
 int _putchar(char c);
 void print_most_numbers(void);
 
