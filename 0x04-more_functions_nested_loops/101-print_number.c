d33454471bd00a690cc7695e9b69cd6a725ff8a1
dev-tch <tijeni.chaabeni@gmail.com>
2023-06-24 02:04:40 +0000

    test 51

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,25 +5,26 @@
  * Return: void
  */
 int getBase(int n);
+int getBase2(int n);
 void print_number(int n)
 {
 int sign = 0;
 int maxbase;
 int res;
+if (n > 0)
 maxbase = getBase(n);
 if (n < 0)
 {
 sign = 1;
-n *= -1;
+maxbase = getBase2(n);
 }
-
 if (sign == 1)
 _putchar('-');
-if (n <= 9)
+if (n <= 9 && n >= -9)
 _putchar(n + '0');
 else
 {
-while (maxbase  > 0)
+while ((sign == 0 && maxbase  > 0) || (sign == 1 && maxbase < 0))
 {
 res = n / maxbase;
 _putchar(res + '0');
@@ -61,5 +62,36 @@ else if (n >= 100)
 maxbase = 100;
 else
 maxbase = 10;
+return (maxbase);
+}
+
+/**
+ * getBase2 - return the most multiple of 10
+ *@n: int
+ * Return: int
+ */
+
+
+int getBase2(int n)
+{
+int maxbase;
+if (n <= -1000000000)
+maxbase = -1000000000;
+else if (n <= -100000000)
+maxbase = -100000000;
+else if (n <= -10000000)
+maxbase = -10000000;
+else if (n <= -1000000)
+maxbase = -1000000;
+else if (n <= -100000)
+maxbase = -100000;
+else if (n <= -10000)
+maxbase = -10000;
+else if (n <= -1000)
+maxbase = -1000;
+else if (n <= -100)
+maxbase = -100;
+else
+maxbase = -10;
 return (maxbase);
 }
