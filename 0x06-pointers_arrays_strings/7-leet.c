fa2d6f059e5bfdc7e728c3f23e95f0f85a22c2f4
dev-tch <tijeni.chaabeni@gmail.com>
2023-06-29 15:32:06 -0700

    program 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,20 @@
 char *leet(char *str)
 {
 char *res  = str;
+char *code = "AEOTL";
+char *tocode   = "43071";
 while (*str)
 {
-if (*str == 'a' || *str == 'A')
-*str = '4';
-else if (*str == 'e' || *str == 'E')
-*str = '3';
-else if (*str == 'o' || *str == 'O')
-*str = '0';
-else if (*str == 't' || *str == 'T')
-*str = '7';
-else if (*str == 'l' || *str == 'L')
-*str = '1';
-else
-*str = *str;
-str =  str + 1;
+while (*code)
+{
+if (*str == *code || *str == (*code + 32))
+*str    =  *tocode;
+code    = code + 1;
+tocode  = tocode + 1;
+}
+code = code - 5;
+tocode = tocode - 5;
+str = str + 1;
 }
 return (res);
 }
