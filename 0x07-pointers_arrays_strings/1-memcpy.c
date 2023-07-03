69f7c2b8f55ce0f840c27fd413f6c9f085d914bd
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-03 10:51:16 -0700

    test2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,11 +8,12 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
+char *res = dest;
 for (i = 0 ; i < n ; i++)
 {
 *dest = src[i];
 dest++;
 }
-dest--;
-return (dest);
+/*dest--;*/
+return (res);
 }
