3d84b9e4409d2846dd74a3fd8b50eacf7f3f86cf
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-05 17:57:34 -0700

    test60

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,26 @@
 /**
+* check_prime - helper function
+* @n: int
+* @i: int
+*Return: int
+*/
+int check_prime(int n, int i)
+{
+if (n <= 1)
+return (0);
+
+if (n % i == 0 && i > 1)
+return (0);
+if ((n / i) < i)
+return (1);
+return (check_prime(n, i + 1));
+}
+/**
  * is_prime_number - check prime number
  * @n: number
  * Return: number
  */
 int is_prime_number(int n)
 {
-int i;
-if (n < 2)
-return (0);
-for (i = 3 ; i * i <= n ; i++)
-{
-if (n % i != 0)
-return (0);
-}
-return (1);
+return (check_prime(n, 1));
 }
