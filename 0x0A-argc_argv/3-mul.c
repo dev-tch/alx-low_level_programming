df3c648d16ec86de3b2730371e3d1a22da76a182
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-07 09:46:14 -0700

    mult two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0
+ *Return: 0
  */
 int main(int argc, char *argv[])
 {
+int mult = 1;
+int nb1  = 1;
+int nb2  = 1;
 if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
-int mult = 1;
-mult = argv[1] * argv[2];
+nb1 = atoi(argv[1]);
+nb2 = atoi(argv[2]);
+if (nb1 == 0 || nb2 == 0)
+{
+printf("Error\n");
+return (1);
+}
+mult = nb1 *nb2;
 printf("%d\n", mult);
 return (0);
 }
