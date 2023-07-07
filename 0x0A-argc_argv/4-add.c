df7bc4cefc5a26d836e90964147d3e2a3ceb4fbc
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-07 10:57:29 -0700

    add numbers test2

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,16 +10,16 @@ int main(int argc, char *argv[])
 {
 int sum = 0;
 int i;
+char *ptr_at_first_not__digit;
 int nb;
 for (i = 1 ; i < argc ; i++)
 {
-nb = atoi(argv[i]);
-if (nb == 0)
+nb = strtol(argv[i], &ptr_at_first_not_digit, 10);
+if (*ptr_at_first_not_digit)
 {
 printf("Error\n");
 return (1);
 }
-if (nb > 0)
 sum = sum + nb;
 }
 printf("%d\n", sum);
