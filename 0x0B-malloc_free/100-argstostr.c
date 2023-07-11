d8d099fbe1d57afaf263f150e23cfc058213f214
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-11 19:36:18 +0000

    new version argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,76 +1,15 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
-* concat_str - concat two strings
-*@s1: string1
-*@s2: string2
-*Return: pointer to newly allocated string
+* get_len - rrecursive functuion to return len of string
+* @str: string
+* Return: int
 */
-char *concat_str(char *s1, char *s2)
+int get_len(char *str)
 {
-char *str_res;
-char *bkp1, *bkp2;
-int i = 0, len1 = 0, len2 = 0;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-bkp1 = s1;
-bkp2 = s2;
-while (*s1)
-{
-len1++;
-s1++;
-}
-while (*s2)
-{
-len2++;
-s2++;
-}
-str_res = (char *) malloc((len1 + len2 + 1) * sizeof(char));
-if (str_res == NULL)
-return (NULL);
-while (i < len1)
-{
-str_res[i] = bkp1[i];
-i++;
-}
-while (i < (len1 + len2))
-{
-str_res[i] = bkp2[i - len1];
-i++;
-}
-
-str_res[len1 + len2] = '\0';
-return (str_res);
-}
-/**
-*copy_str - new pointer to copied string
-*@str: string
-*Return: pointer to string
-*/
-char *copy_str(char *str)
-{
-char *cpy = NULL;
-char *bkp = str;
-int len = 0, i;
-if (str == NULL)
-return (NULL);
-/*get the length*/
-while (*str)
-{
-len++;
-str++;
-}
-/* memory allocation*/
-cpy = (char *) malloc((len + 1) * sizeof(char));
-if (cpy == NULL)
-return (NULL);
-for (i = 0 ; i < len ; i++)
-{
-cpy[i] = bkp[i];
-}
-return (cpy);
+if (*str == '\0')
+return (0);
+return (1 + get_len(str + 1));
 }
 /**
 *argstostr - concatenate all arguments of program
@@ -80,16 +19,26 @@ return (cpy);
 */
 char *argstostr(int ac, char **av)
 {
-int i;
+int i = 0, j = 0, k = 0, total_length = 0;
 char *s = NULL;
 if (ac == 0 || av == NULL)
 return (NULL);
-s = copy_str(av[0]);
-s = concat_str(s, "\n");
-for (i = 1 ; i < ac ; i++)
+/*memory allocation*/
+for (i = 0; i < ac; i++)
+total_length += get_len(av[i]) + 1;
+s = malloc(sizeof(char) * (total_length + 1));
+for (i = 0 ; i < ac ; i++)
 {
-s = concat_str(s, av[i]);
-s = concat_str(s, "\n");
+j = 0;
+while (av[i][j] != '\0')
+{
+s[k] = av[i][j];
+k++;
+j++;
 }
+s[k] = '\n';
+k++;
+}
+s[k] = '\0';
 return (s);
 }
