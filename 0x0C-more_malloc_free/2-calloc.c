bb3fab6bdb117032b0f143ce61b57a509ca8f13e
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-13 20:41:24 +0000

    test3-task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,18 +8,21 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *ptr = NULL;
-char *bkp = NULL;
+unsigned char *byte_ptr = NULL;
+unsigned int total = 0;
+unsigned int i = 0;
+void *ptr = NULL;
 if (nmemb == 0 || size == 0)
 return (NULL);
 ptr = (char *) malloc(nmemb * size);
 if (ptr == NULL)
 return (NULL);
-bkp = ptr;
-while (*ptr)
+total = nmemb * size;
+byte_ptr = (unsigned char *)ptr;
+while (i < total)
 {
-*ptr = '0';
-ptr++;
+byte_ptr[i] = 0;
+i++;
 }
-return (bkp);
+return (ptr);
 }
