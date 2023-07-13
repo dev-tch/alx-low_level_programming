76a369e30fa871b5badb1342d2c09e888a6e7f72
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-13 19:11:30 +0000

    test task1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stddef.h>
+/**
+* get_len - return len of string
+* @str: string
+* Return: int
+*/
+int get_len(char *str)
+{
+if (*str == '\0' || str == NULL)
+return (0);
+return (1 + get_len(str + 1));
+}
+
+/**
+* string_nconcat - concatenates two strings
+*@s1: string1
+*@s2: string2
+*@n: n bytes of string2
+*Return: pointer to concatenad string
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *ptr  = NULL;
+char *bkp1 = s1;
+char *bkp2 = s2;
+char *bkp3 = NULL;
+unsigned int size_s1 = 0;
+unsigned int size_s2 = 0;
+unsigned int i       = 0;
+size_s1 = get_len(bkp1);
+size_s2 = get_len(bkp2);
+if (n >= size_s2)
+n = size_s2;
+ptr = (char *) malloc((size_s1 + n + 1) * sizeof(char));
+if (ptr == NULL)
+return (NULL);
+bkp3 = ptr;
+while (i < size_s1)
+{
+*ptr = *s1;
+ptr++;
+s1++;
+i++;
+}
+while (i < (n + size_s1))
+{
+*ptr = *(s2 + i - size_s1);
+ptr++;
+i++;
+}
+*ptr = '\0';
+
+return (bkp3);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,1 +1,2 @@
 add program 0-malloc_checked.c
+add program 1-string_nconcat.c
