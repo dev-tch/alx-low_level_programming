62278982d0d313bbef197fec055062ee24876012
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-13 20:11:21 +0000

    test task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stddef.h>
+/**
+* _calloc - allocates memory for an array
+* @nmemb: number of elements
+* @size: size of element
+* Return: pointer to allocated memory
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *ptr = NULL;
+char *bkp = NULL;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = (char *) malloc(nmemb * size);
+bkp = ptr;
+if (ptr == NULL)
+return (NULL);
+while (*ptr)
+{
+*ptr = '0';
+ptr++;
+}
+return (bkp);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,2 +1,3 @@
 add program 0-malloc_checked.c
 add program 1-string_nconcat.c
+add program 2-calloc.c
