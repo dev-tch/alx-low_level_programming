ed2c4f611d68ca63ef4461eaceffe6b95e8c303c
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-13 19:28:30 +0000

    test3- task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,7 @@
 */
 int get_len(char *str)
 {
-if (*str == '\0' || str == NULL)
+if (*str == '\0')
 return (0);
 return (1 + get_len(str + 1));
 }
@@ -28,6 +28,10 @@ char *bkp3 = NULL;
 unsigned int size_s1 = 0;
 unsigned int size_s2 = 0;
 unsigned int i       = 0;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 size_s1 = get_len(bkp1);
 size_s2 = get_len(bkp2);
 if (n >= size_s2)
@@ -36,17 +40,17 @@ ptr = (char *) malloc((size_s1 + n + 1) * sizeof(char));
 if (ptr == NULL)
 return (NULL);
 bkp3 = ptr;
-while (i < size_s1)
+while (*s1)
 {
 *ptr = *s1;
 ptr++;
 s1++;
-i++;
 }
-while (i < (n + size_s1))
+while (i < n  &&  *s2)
 {
-*ptr = *(s2 + i - size_s1);
+*ptr = *s2;
 ptr++;
+s2++;
 i++;
 }
 *ptr = '\0';
