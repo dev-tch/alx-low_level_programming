a7b552d5f782c7413fdb224862b7248ee39c633b
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-17 21:24:37 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef H_DEF
+#define H_DEF DOG_H
+/**
+* struct dog - new class dog
+* @name: attribute
+* @age : attribute
+* @owner:attribute
+*  Description: hold the state of object dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
