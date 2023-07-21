064e5b697d9e253d67fd468433d14e979ec570cc
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-21 00:00:21 +0000

    repair header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef H_VA
-#define H_VA
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
