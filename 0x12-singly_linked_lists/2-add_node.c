ad2084c2dad9396640fcc791ddcdabf82c5963a1
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-27 23:52:22 +0000

    fix  task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,13 +9,24 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *new;
 new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
+if (str != NULL)
+{
 new->str = strdup(str);
 new->len = strlen(new->str);
+}
+else
+{
+new->str = NULL;
+new->len = 0;
+}
 if (head != NULL)
 {
 new->next = *head;
 *head = new;
 }
+else
 head = &new;
 return (new);
 }
