9deb4fa0e4cee3d5586ddafdf7271f7722f3ab44
dev-tch <tijeni.chaabeni@gmail.com>
2023-07-31 21:09:37 +0000

    task5 linkedlist

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* free_listint2 - free the list
+* @head: pointer of pointers to nodes
+* Return: void
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+if (head == NULL)
+return;
+while (*head != NULL)
+{
+temp = *head;
+*head = (*head)->next;
+free(temp);
+}
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -3,3 +3,5 @@
 - add 2-add_nodeint.c
 - add 3-add_nodeint_end.c
 - add 4-free_listint.c
+- add 5-free_listint2.c
+
