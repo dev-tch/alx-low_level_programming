1282e16b191b0cbd517b5b4d282ab33a3b522848
dev-tch <tijeni.chaabeni@gmail.com>
2023-08-03 21:05:28 +0000

    fix7 task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,41 +1,24 @@
-#include "main.h"
 /**
-* len_string - calculate the length of string
-* @str: string
-* Return: length of string
-*/
-unsigned long int len_string(const char *str)
-{
-if (*str == '\0')
-return (0);
-return (1 + len_string(str + 1));
-}
-/**
-* binary_to_uint - convert binary string to int
-* @b: binary string
-* Return: unsigned int
-*/
+ * binary_to_uint - convert binary string to int
+ * @b: binary string
+ *
+ * Return: int (unsigned int)
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned long int nb_digit = 0;
-unsigned long int sum = 0;
-unsigned long int factor = 0;
-long int i = 0;
+	unsigned int nb;
+	unsigned int i;
 
-nb_digit  = len_string(b);
-/*check b is null*/
-if (b == NULL || *b == '\0')
-return (0);
-for (i = nb_digit - 1 ; i >= 0; i--)
-{
-if (b[i] != '0' &&  b[i] != '1')
-{
-return (0);
-}
-if (b[i] == '1')
-sum += 1 << factor;
-factor++;
-}
-return (sum);
+	for (nb = 0, i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '1')
+			nb = (nb << 1) | 1;
+		else if (b[i] == '0')
+			nb <<= 1;
+		else if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	return (nb);
 }
