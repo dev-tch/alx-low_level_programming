665778d57c179846849a8c2718608f70e76b8dc8
dev-tch <tijeni.chaabeni@gmail.com>
2023-08-04 00:03:23 +0000

    fix get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,14 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned long  mask;
+unsigned long nb_bits;
 int res;
+nb_bits = sizeof(n) * 8;
+/*is outbound index*/
+if (index >= nb_bits)
+{
+return (-1);
+}
 mask = 1UL << index;
 res = (mask & n) ? 1 : 0;
 return (res);
