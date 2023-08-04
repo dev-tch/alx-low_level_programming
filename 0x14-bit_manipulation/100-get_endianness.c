82283c861a90982036f7146231928ad6684ff7bb
dev-tch <tijeni.chaabeni@gmail.com>
2023-08-04 18:01:58 +0000

    task 100 bit manip

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#define BIG_END  0
+#define LIT_END  1
+/**
+* get_endianness - determine the type of ordering bytes
+* Return : (0 or 1)
+*/
+int get_endianness(void)
+{
+short int data = 0x0001;
+char *ptr = (char *)&data;
+return (ptr[0] ? LIT_END : BIG_END);
+}
