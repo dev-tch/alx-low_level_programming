0e798f4b7c12c0effe57552c4ebc66f46a9cfd86
dev-tch <tijeni.chaabeni@gmail.com>
2023-08-07 20:48:01 +0000

    fix task0 FILEIO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,28 +9,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *msg;
 ssize_t  nb_reads, ret, ret_close, file_des;
-if (filename == NULL)
+if (!filename || !letters)
 return (0);
 file_des = open(filename, O_RDONLY);
-if (file_des == -1)
+if (file_des < 0)
 {
 return (0);
 }
 msg = (char *) malloc(letters);
-if (msg == NULL)
+if (!msg)
 return (0);
 nb_reads = read(file_des, msg, letters);
-if (nb_reads ==  0)
+if (nb_reads <  0)
+{
+free(msg);
 return (0);
+}
 msg[letters] = '\0';
 
-ret = write(STDOUT_FILENO, msg, letters);
-if (ret == -1)
+ret = write(STDOUT_FILENO, msg, nb_reads);
+if (ret <= 0)
+{
+free(msg);
 return (0);
+}
 /*clean up memory */
 free(msg);
 ret_close = close(file_des);
 if (ret_close == -1)
 return (0);
-return (ret);
+return (nb_reads);
 }
