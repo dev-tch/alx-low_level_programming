b5e9015f5c60f78aa07f7c0aecb62db7debbe017
dev-tch <tijeni.chaabeni@gmail.com>
2023-09-22 22:34:27 +0000

    new test crack jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/dev-tch/alx-low_level_programming/tree/master/0x18-dynamic_libraries/inject_code.so
+wget -P ../ https://github.com/dev-tch/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject_code.so
 export LD_PRELOAD=../inject_code.so
